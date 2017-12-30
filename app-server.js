cbbc4f6b1641ba204de7450cbc567ad2a7637c07
Harith Zahid <haris.wdev@gmail.com>
2017-12-30 15:58:55 +0800

    Heroku setup

--- app-server.js
+++ app-server.js
@@ -19,7 +19,7 @@ var results = {
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 5000);
 var io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
