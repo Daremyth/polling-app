8b0d1e62ccbec503b3d0e6f8278d257370b69d98
Harith Zahid <haris.wdev@gmail.com>
2017-12-30 12:39:09 +0800

    Build for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
+	devtool: 'cheap-module-source-map',
 	entry: "./app-client.js",
 	output: {
 		filename: "public/bundle.js"
 	},
+	plugins: [
+		new webpack.DefinePlugin({
+		  'process.env': {
+			'NODE_ENV': JSON.stringify('production')
+		  }
+		})
+	  ],
 	module: {
 		loaders: [
 			{
